1240075706fcfd832901159bd1b0c02c10caf9a8
Eren Araz <lerenarazl@gmail.com>
2026-08-30 00:34:12 +0300

    Setup 2048hl chonker net training

--- examples/simple.rs
+++ examples/simple.rs
@@ -16,7 +16,7 @@ use bullet_lib::{
     value::{ValueTrainerBuilder, loader},
 };
 
-const HIDDEN_SIZE: usize = 1024;
+const HIDDEN_SIZE: usize = 2048;
 const NUM_OUTPUT_BUCKETS: usize = 8;
 const SCALE: i32 = 400;
 const QA: i16 = 255;
@@ -91,7 +91,7 @@ fn main() {
     let superbatches = 480;
 
     let schedule = TrainingSchedule {
-        net_id: "potential-1024hl-2".to_string(),
+        net_id: "potential-2048hl-chonker".to_string(),
         eval_scale: SCALE as f32,
         steps: TrainingSteps {
             batch_size: 16_384,
